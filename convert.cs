364d822926e109c2c9c0cf36d19bba0792b50ede
Leol22 <99835836+Leol22@users.noreply.github.com>
2023-02-07 23:11:32 +0100

    removed the redundant useless "using" commands

--- convert.cs
+++ convert.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Metadata.Ecma335;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Capsule
 {
     public class gridmath
